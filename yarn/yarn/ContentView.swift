be091d15c5342a39ae629f9a5d21083ab9e15de1
Bora <borabalos9@gmail.com>
2024-12-12 17:14:02 +0100

    Scroll view styling and mapping fake data

--- yarn/yarn/ContentView.swift
+++ yarn/yarn/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    var messageArray = ["Hi mate!", "How are you doing?", "Isn't Apple the best ecosystem? It's just so clean", "Innit?"]
     var body: some View {
         VStack {
             TitleRow()
+            
+            ScrollView {
+                ForEach(messageArray, id: \.self) {
+                    text in MessageBubble(message: Message(id: "12345", text: text, received: true, timestamp: Date()))
+                }
+            }
+            .padding(.top, 10)
+            .background(.white)
         }
         .background(Color("Peach"))
     }
