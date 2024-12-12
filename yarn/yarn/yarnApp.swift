5e36d95696721bb57600ed6ff5f9642f2092414a
Bora <borabalos9@gmail.com>
2024-12-12 12:14:00 +0100

    Initialize project

---
+++ yarn/yarn/yarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  yarnApp.swift
+//  yarn
+//
+//  Created by Bora Balos on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct yarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ yarn/yarnTests/yarnTests.swift
@@ -0,0 +1,17 @@
+//
+//  yarnTests.swift
+//  yarnTests
+//
+//  Created by Bora Balos on 12/12/2024.
+//
+
+import Testing
+@testable import yarn
+
+struct yarnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
